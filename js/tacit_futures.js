d5071e20821a6e9b972e5f631bb1442990aea95f
harlo <harlo.holmes@gmail.com>
2016-10-27 09:36:23 -0400

    stubs for timeline

--- js/tacit_futures.js
+++ js/tacit_futures.js
@@ -1,3 +1,36 @@
 (function($){
+	/*
+	 * init timeline
+	 */
+
+	var startTime = 0;	// sept 2015
+	var endTime = 0;	// nov 2015
+
+	/*
+	 * init map
+	 */
+
+	var tileLayer = "";	// url of tile layer
+	var mapCenter = [];	// lat-lng of map center
+	var mapZoom = 10;	// initial zoom
+
+	var map = new L.Map('tf_map_holder');
+	var  = new L.TileLayer();
+
+	map.setView(mapCenter, mapZoom);
+	map.addLayer(new L.TileLayer(tileLayer));
+
+	/*
+	 * init player
+	 */
+
+	 var geoJson = null;	// load our GeoJSON
+	 var onPlaybackTimeChange = function(ms) {};
+	 var opts = {
+	 	playControl: true,
+	 	dateControl: true
+	 };
+
+	 var player = new L.Playback(map, geoJson, onPlaybackTimeChange, opts);
 
 })(jQuery);
